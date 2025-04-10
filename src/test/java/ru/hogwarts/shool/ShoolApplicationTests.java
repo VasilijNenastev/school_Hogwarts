afbe821eb3f1774ba469cb7e27cc35aed897782a
1 <vasilijnenastev@gmail.com>
2025-04-10 12:18:40 +0400

    Install Commit

---
+++ src/test/java/ru/hogwarts/shool/ShoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.shool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
